64179c21f7e25dc52c50b551c8d0a14c6906d2d8
Camoralesh27 <camoralesh27@gmail.com>
2025-09-29 00:09:21 -0600

    creando secciones con estilos hero, nosotros, actividades, contacto, footer

--- src/js/app.js
+++ src/js/app.js
@@ -39,67 +39,7 @@ showMenu('navM-toggle', 'navM-menu');
 
 
 /*==================== CAMBIAR IDIOMA ====================*/
-const flagsElement = document.querySelector('#flags');
-const textsToChange = document.querySelectorAll('[data-section]');
-const inputName = document.querySelector('#name');
-const inputEmail = document.querySelector('#email');
-const inputMessage = document.querySelector('#message');
 
-// Función para cambiar el idioma y guardar la preferencia en localStorage
-const setLanguage = (language) => {
-    flagsElement.setAttribute('data-language', language);
-    localStorage.setItem('selectedLanguage', language);
-    changeLanguages(language);
-    
-    inputName.placeholder = language === 'esp' ? 'Tu nombre' : 'Your name';
-    inputEmail.placeholder = language === 'esp' ? 'Tu correo electrónico' : 'Your email';
-    inputMessage.placeholder = language === 'esp' ? 'La descripción de tu proyecto' : 'Your project description';
-};
 
-// Escuchar el cambio de checkbox y actualizar idioma
-flagsElement.addEventListener('change', (e) => {
-    let language = e.target.checked ? 'eng' : 'esp';
-    setLanguage(language);
-});
-
-// Cargar el idioma guardado al cargar la página
-document.addEventListener("DOMContentLoaded", () => {
-    let savedLanguage = localStorage.getItem('selectedLanguage') || 'esp'; // Idioma predeterminado: inglés
-    flagsElement.checked = savedLanguage === 'eng';
-    setLanguage(savedLanguage);
-});
-
-// Toma el JSON y modifica el idioma
-const changeLanguages = async (language) => {
-    const requestJson = await fetch(`./languages/${language}.json`);
-    const texts = await requestJson.json();
-
-    for (const textToChange of textsToChange) {
-        const section = textToChange.dataset.section;
-        const value = textToChange.dataset.value;
-        
-        /* textToChange.innerHTML = texts[section][value]; */
-
-        let keys = value.split('.');
-        let translation = texts[section];
-
-        for (const key of keys) {
-            if (translation && key in translation) {
-                translation = translation[key];
-            } else {
-                translation = undefined;
-                break;
-            }
-        }
-
-        if (translation !== undefined) {
-            textToChange.innerHTML = translation;
-        } else {
-            console.warn(`⚠️ Traducción no encontrada para ${section}.${value}`);
-        }
-
-    }
-};
-
-/*======= Actualizar año footer =======*/
+/*======= ACTUALIZAR AÑO FOOTER =======*/
 document.getElementById("year").textContent = new Date().getFullYear();
